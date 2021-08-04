38f63827f233813ee407d96f2aaaf654bfc2bc1d
Mayra Rodriguez <mayrarodriguez1709@gmail.com>
2021-08-04 15:06:38 -0500

    chore: install firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: process.env.FIREBASE_API_KEY ?? null,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN ?? null,
+    databaseURL: process.env.FIREBASE_DATABASE_URL ?? null,
+    projectId: process.env.FIREBASE_PROJECT_ID ?? null,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET ?? null,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID ?? null,
+    appId: process.env.FIREBASE_APP_ID ?? null
+  }
 };
 
 /*
